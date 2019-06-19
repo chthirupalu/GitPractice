219b905ff4119d8712efae7cfecb075a40a5a666
cthirupalu <cthirupalu@apple.com>
2019-06-19 16:44:32 +0530

    initial code commit

---
+++ PracticeGit/src/main/java/com/thiru/practice/pojo/Test.java
@@ -0,0 +1,32 @@
+package com.thiru.practice.pojo;
+
+public class Test {
+
+	private int no;
+	private String name;
+	/**
+	 * @return the no
+	 */
+	public int getNo() {
+		return no;
+	}
+	/**
+	 * @param no the no to set
+	 */
+	public void setNo(int no) {
+		this.no = no;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ PracticeGit/src/main/java/com/thiru/practice/pojo/Test2.java
@@ -0,0 +1,32 @@
+package com.thiru.practice.pojo;
+
+public class Test2 {
+
+	private int no1;
+	private int no2;
+	/**
+	 * @return the no1
+	 */
+	public int getNo1() {
+		return no1;
+	}
+	/**
+	 * @param no1 the no1 to set
+	 */
+	public void setNo1(int no1) {
+		this.no1 = no1;
+	}
+	/**
+	 * @return the no2
+	 */
+	public int getNo2() {
+		return no2;
+	}
+	/**
+	 * @param no2 the no2 to set
+	 */
+	public void setNo2(int no2) {
+		this.no2 = no2;
+	}
+	
+}
